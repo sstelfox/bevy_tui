665857c4437a9bc3b8b51b2eae01de87377efcf1
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-01-04 14:23:52 -0500

    handle more input values

--- src/adapted_input.rs
+++ src/adapted_input.rs
@@ -57,56 +57,78 @@ fn convert_input_kind(kind: crossterm::event::KeyEventKind) -> Option<ButtonStat
     }
 }
 
+macro_rules! shifted {
+    ($key_code:expr) => { vec![KeyCode::LShift, $key_code] }
+}
+
+macro_rules! unshifted {
+    ($key_code:expr) => { vec![$key_code] }
+}
+
 fn convert_key_code(key_code: crossterm::event::KeyCode) -> Vec<KeyCode> {
     use crossterm::event::KeyCode::*;
 
-    let mut key_codes = vec![];
-
-    match key_code {
+    let key_codes = match key_code {
         // what a dumb enum variant name... There is a button dedicated to 'back' as a media key...
         // Why not use the actual name?
-        Backspace => key_codes.push(KeyCode::Back),
+        Backspace => vec![KeyCode::Back],
         Char(ch) => {
             match ch {
-                '1' => key_codes.push(KeyCode::Key1),
-                '2' => key_codes.push(KeyCode::Key2),
-                '3' => key_codes.push(KeyCode::Key3),
-                '4' => key_codes.push(KeyCode::Key4),
-                '5' => key_codes.push(KeyCode::Key5),
-                '6' => key_codes.push(KeyCode::Key6),
-                '7' => key_codes.push(KeyCode::Key7),
-                '8' => key_codes.push(KeyCode::Key8),
-                '9' => key_codes.push(KeyCode::Key9),
-                '0' => key_codes.push(KeyCode::Key0),
-                'a' => key_codes.push(KeyCode::A),
-                'A' => {
-                    key_codes.push(KeyCode::A);
-                    key_codes.push(KeyCode::LShift);
-                }
+                '`' => unshifted!(KeyCode::Grave),
+                '~' => shifted!(KeyCode::Grave),
+                '1' => unshifted!(KeyCode::Key1),
+                '!' => shifted!(KeyCode::Key1),
+                '2' => unshifted!(KeyCode::Key2),
+                '@' => shifted!(KeyCode::Key2),
+                '3' => unshifted!(KeyCode::Key3),
+                '#' => shifted!(KeyCode::Key3),
+                '4' => unshifted!(KeyCode::Key4),
+                '$' => shifted!(KeyCode::Key4),
+                '5' => unshifted!(KeyCode::Key5),
+                '%' => shifted!(KeyCode::Key5),
+                '6' => unshifted!(KeyCode::Key6),
+                '^' => shifted!(KeyCode::Key6),
+                '7' => unshifted!(KeyCode::Key7),
+                '&' => shifted!(KeyCode::Key7),
+                '8' => unshifted!(KeyCode::Key8),
+                '*' => shifted!(KeyCode::Key8),
+                '9' => unshifted!(KeyCode::Key9),
+                '(' => shifted!(KeyCode::Key9),
+                '0' => unshifted!(KeyCode::Key0),
+                ')' => shifted!(KeyCode::Key0),
+                '-' => unshifted!(KeyCode::Minus),
+                '_' => shifted!(KeyCode::Minus),
+                '=' => unshifted!(KeyCode::Equals),
+                // It's worth calling out here that there is a Bevy key code for `+` but it is
+                // intended for use with the keypad. I'm prioritizing a more commonly typed
+                // variation (this may be a personal logical fallacy but it works for me). If you
+                // hit an issue regarding this there is a specific published event with the raw
+                // character and I'm open to issues to discuss this further.
+                '+' => shifted!(KeyCode::Equals),
                 // todo: all the typeable keyboard characters...
                 _ => {
                     unimplemented!()
                 }
             }
         }
-        Esc => key_codes.push(KeyCode::Escape),
+        Esc => vec![KeyCode::Escape],
         F(num) => {
             match num {
-                1 => key_codes.push(KeyCode::F1),
-                2 => key_codes.push(KeyCode::F2),
-                3 => key_codes.push(KeyCode::F3),
-                4 => key_codes.push(KeyCode::F4),
-                5 => key_codes.push(KeyCode::F5),
-                6 => key_codes.push(KeyCode::F6),
-                7 => key_codes.push(KeyCode::F7),
-                8 => key_codes.push(KeyCode::F8),
-                9 => key_codes.push(KeyCode::F9),
-                10 => key_codes.push(KeyCode::F10),
-                11 => key_codes.push(KeyCode::F11),
-                12 => key_codes.push(KeyCode::F12),
-                13 => key_codes.push(KeyCode::F13),
-                14 => key_codes.push(KeyCode::F14),
-                15 => key_codes.push(KeyCode::F15),
+                1 => vec![KeyCode::F1],
+                2 => vec![KeyCode::F2],
+                3 => vec![KeyCode::F3],
+                4 => vec![KeyCode::F4],
+                5 => vec![KeyCode::F5],
+                6 => vec![KeyCode::F6],
+                7 => vec![KeyCode::F7],
+                8 => vec![KeyCode::F8],
+                9 => vec![KeyCode::F9],
+                10 => vec![KeyCode::F10],
+                11 => vec![KeyCode::F11],
+                12 => vec![KeyCode::F12],
+                13 => vec![KeyCode::F13],
+                14 => vec![KeyCode::F14],
+                15 => vec![KeyCode::F15],
                 _ => {
                     // do these others actually exist?
                     unimplemented!()
@@ -117,7 +139,7 @@ fn convert_key_code(key_code: crossterm::event::KeyCode) -> Vec<KeyCode> {
         _ => {
             unimplemented!()
         }
-    }
+    };
 
     key_codes
 }
